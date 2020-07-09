3d11c077e2a60b818c369f44ca8e2c1b2182069b
david2203 <david.boes22@gmail.com>
2020-07-09 09:54:33 +0200

    First Commit

---
+++ src/programm.java
@@ -0,0 +1,20 @@
+public class programm
+{
+    public static void main( String[] args )
+{
+    System.out.println( "Schachfarben lernen" );
+    System.out.println( "Grundsätzlich gilt für die erste Reihe (A1-H1): ungerade = schwarz, gerade = weiß" );
+    System.out.println( "Mit ungerade und gerade sind die Buchstaben gemeint (A=ungerade, B=gerade, C=ungerade)" );
+    System.out.println( "Es gibt 4 verschiedene Kombinationen." );
+    System.out.println( "gerade und gerade = Change" );
+    System.out.println( "gerade ungerade richtig" );
+    System.out.println( "Erklärung:" );
+    System.out.println( "Erklärung:" );
+    System.out.println( "Erklärung:" );
+    System.out.println( "Oder ihr habt das Brett grundsätzlich vor eurem geistigen Auge:" );
+
+
+}
+
+
+}
